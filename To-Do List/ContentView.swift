ce5dc9914a5e1ed06ccf7f26bdb9d18c8343ecc8
Vader20FF <229898@edu.p.lodz.pl>
2021-07-22 00:15:24 +0200

    - Added fully functioning AddTaskView

--- To-Do List/ContentView.swift
+++ To-Do List/ContentView.swift
@@ -10,6 +10,15 @@ import CoreData
 
 let daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
+let dateRange: ClosedRange<Date> = {
+    let calendar = Calendar.current
+    let startComponents = DateComponents(year: 2021, month: 1, day: 1)
+    let endComponents = DateComponents(year: 2021, month: 12, day: 31, hour: 23, minute: 59, second: 59)
+    return calendar.date(from:startComponents)!
+        ...
+        calendar.date(from:endComponents)!
+}()
+
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @AppStorage("isDarkMode") private var darkMode = false
@@ -119,6 +128,11 @@ struct AddTaskView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @AppStorage("isDarkMode") private var darkMode = false
     @State var showTaskView = false
+    @State private var itemTitle = ""
+    @State private var itemFullDescription = ""
+    @State private var itemDay = ""
+    @State private var itemDate = Date()
+    @State private var itemFinished = false
     
     var body: some View {
         GeometryReader { geometry in
@@ -127,57 +141,93 @@ struct AddTaskView: View {
             
             VStack {
                 if showTaskView {
-                    withAnimation {
-                        ContentView()
-                    }
+                    ContentView()
                 } else {
-                    
                     TopView()
                         .frame(width: screenWidth, height: screenHeight * 0.1)
                         .padding(.top, screenHeight * 0.025)
-    //
-    //                VStack (spacing: screenHeight * 0.04) {
-    //                    Section(header: Text("TITLE")) {
-    //                        TextField(self.item.title!, text: $item.title ?? "")
-    //                            .padding(.horizontal, screenWidth * 0.05)
-    //                    }
-    //
-    //                    Section(header: Text("DESCRIPTION")) {
-    //                        TextField(self.item.fullDescription!, text: $item.fullDescription ?? "")
-    //                            .padding(.horizontal, screenWidth * 0.05)
-    //                    }
-    //
-    //                    Section(header: Text("DAY")) {
-    //                        Text(self.item.day!)
-    //                            .foregroundColor(.blue)
-    //                    }
-    //
-    //                    Section(header: Text("DATE")) {
-    //                        DatePicker("", selection: $item.date ?? Date(), in: dateRange, displayedComponents: [.date])
-    //                            .padding(.trailing, screenWidth * 0.37)
-    //                    }
-    //
-    //                    Section(header: Text("FINISHED")) {
-    //                        Toggle("", isOn: $item.finished)
-    //                            .padding(.trailing, screenWidth * 0.45)
-    //                    }
-    //                }
-    //                .textFieldStyle(RoundedBorderTextFieldStyle())
                     
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 25, style: .continuous)
-                            .fill(Color.red)
-                            .frame(width: screenWidth * 0.3, height: screenHeight * 0.05)
+                    Spacer()
+                    
+                    VStack (spacing: screenHeight * 0.04) {
+                        Section(header: Text("TITLE")) {
+                            TextField("", text: $itemTitle)
+                                .padding(.horizontal, screenWidth * 0.05)
+                        }
                         
-                        Button(action: { showTaskView = true }) {
-                            Label("Save Task", systemImage: "")
-                                .foregroundColor(.white)
+                        Section(header: Text("DESCRIPTION")) {
+                            TextField("", text: $itemFullDescription)
+                                .padding(.horizontal, screenWidth * 0.05)
+                        }
+                        
+                        Section(header: Text("DAY")) {
+                            Text("")
+                                .foregroundColor(.blue)
+                        }
+                        
+                        Section(header: Text("DATE")) {
+                            DatePicker("", selection: $itemDate, in: dateRange, displayedComponents: [.date])
+                                .padding(.trailing, screenWidth * 0.37)
+                        }
+                        
+                        Section(header: Text("FINISHED")) {
+                            Toggle("", isOn: $itemFinished)
+                                .padding(.trailing, screenWidth * 0.45)
+                        }
+                    }
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Spacer()
+                    
+                    HStack (spacing: screenWidth * 0.1) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                                .fill(Color.red)
+                                .frame(width: screenWidth * 0.3, height: screenHeight * 0.05)
+                            
+                            Button(action: { showTaskView = true }) {
+                                Label("Cancel", systemImage: "")
+                                    .foregroundColor(.white)
+                                    .padding(.trailing, screenWidth * 0.02)
+                            }
+                        }
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                                .fill(Color.green)
+                                .frame(width: screenWidth * 0.3, height: screenHeight * 0.05)
+                            
+                            Button(action: {
+                                let item = Item(context: viewContext)
+                                
+                                item.title = itemTitle
+                                item.fullDescription = itemFullDescription
+                                item.day = getDayOfTheWeekFromDate(passedDate: itemDate)
+                                item.date = itemDate
+                                item.finished = itemFinished
+                                withAnimation {
+                                    saveContext()
+                                    showTaskView = true
+                                }
+                            }) {
+                                Label("Save Task", systemImage: "")
+                                    .foregroundColor(.white)
+                            }
                         }
                     }
                 }
             }
             .preferredColorScheme(darkMode ? .dark : .light)
             .environment(\.colorScheme, darkMode ? .dark : .light)
+        }
+    }
+    
+    private func saveContext() {
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
     }
 }
@@ -188,15 +238,6 @@ struct DetailView: View {
     init(newItem: Item) {
         _item = State(initialValue: newItem)
     }
-    
-    let dateRange: ClosedRange<Date> = {
-        let calendar = Calendar.current
-        let startComponents = DateComponents(year: 2021, month: 1, day: 1)
-        let endComponents = DateComponents(year: 2021, month: 12, day: 31, hour: 23, minute: 59, second: 59)
-        return calendar.date(from:startComponents)!
-            ...
-            calendar.date(from:endComponents)!
-    }()
     
     var body: some View {
         GeometryReader { geometry in
